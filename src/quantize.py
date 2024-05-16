c43bea5e52ba091eb8e5cd0573ff4436333db928
Lukasz Staniszewski <lukaszstaniszewski10@gmail.com>
2024-05-17 00:43:38 +0200

    feat: add quantization

---
+++ src/quantize.py
@@ -0,0 +1,160 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
+
+import hydra
+import lightning as L
+import rootutils
+import torch
+from lightning import Callback, LightningDataModule, LightningModule, Trainer
+from lightning.pytorch.loggers import Logger
+from omegaconf import DictConfig
+
+from utils.quantization import (
+    QuantizerConfig,
+    get_model_quantized,
+    get_model_size_mb,
+    prepare_for_QAT,
+    quantize_fuse_bn,
+    quantize_PTQ,
+)
+
+rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
+from src.utils import (
+    RankedLogger,
+    extras,
+    get_metric_value,
+    instantiate_callbacks,
+    instantiate_loggers,
+    log_hyperparameters,
+    task_wrapper,
+)
+
+log = RankedLogger(__name__, rank_zero_only=True)
+
+
+def _quantize(
+    model: torch.nn.Module, cfg: DictConfig, datamodule: LightningDataModule, trainer: Trainer
+) -> Tuple[torch.nn.Module, Path]:
+    """Performs quantization based on chosen methods."""
+    log.info("Starting quantization...")
+    quant_path = Path(cfg.paths.output_dir) / "quant"
+    if not os.path.exists(quant_path):
+        os.makedirs(quant_path, exist_ok=True)
+    quantization_methods = cfg.quantization.methods
+    quantizer = None
+    ckpt_path = None
+    quant_config = QuantizerConfig(**cfg.quantization.quant_config)
+    if "fuse_bn" in quantization_methods:
+        log.info("FuseBatchNorm Quantization...")
+        model = quantize_fuse_bn(model)
+    if "ptq" in quantization_methods:
+        log.info("Performing PTQ...")
+        device = "cuda" if cfg.trainer.accelerator == "gpu" else "cpu"
+        model, quantizer = quantize_PTQ(
+            model=model,
+            work_dir=quant_path,
+            quant_config=quant_config,
+            n_ptq_examples=cfg.quantization.ptq.examples_limit,
+            device=device,
+            dataloader=datamodule.train_dataloader(),
+            quantizer=quantizer,
+        )
+        log.info("PTQ Finished...")
+    if "qat" in quantization_methods:
+        log.info("Performing QAT...")
+        model, quantizer = prepare_for_QAT(
+            model=model, work_dir=quant_path, quant_config=quant_config, quantizer=quantizer
+        )
+        log.info("Starting training!")
+        trainer.fit(model=model, datamodule=datamodule)
+        ckpt_path = trainer.checkpoint_callback.best_model_path
+        if ckpt_path:
+            model.load_state_dict(ckpt_path)
+        log.info("QAT Finished...")
+
+    quantized_model = get_model_quantized(model=model)
+    model_path = quant_path / f"{'_'.join(quantization_methods)}_qmodel.pth"
+    torch.save(quantized_model.state_dict(), model_path)
+    return quantized_model, model_path
+
+
+@task_wrapper
+def quantize(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    """Quantize the model. After all, it evaluates the model.
+
+    :param cfg: A DictConfig configuration composed by Hydra.
+    :return: A tuple with metrics and dict with all instantiated objects. # TODO
+    """
+    # set seed for random number generators in pytorch, numpy and python.random
+    if cfg.get("seed"):
+        L.seed_everything(cfg.seed, workers=True)
+
+    if cfg.trainer.accelerator == "gpu":
+        torch.set_float32_matmul_precision("medium")
+
+    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
+
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(cfg.model)
+    model.load_from_checkpoint(cfg.get("ckpt_path"))
+
+    log.info(f"Model size before quantization: {get_model_size_mb(model):.3f} MB")
+
+    log.info("Instantiating callbacks...")
+    callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
+
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = instantiate_loggers(cfg.get("logger"))
+
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
+
+    object_dict = {
+        "cfg": cfg,
+        "datamodule": datamodule,
+        "model": model,
+        "callbacks": callbacks,
+        "logger": logger,
+        "trainer": trainer,
+    }
+
+    if logger:
+        log.info("Logging hyperparameters!")
+        log_hyperparameters(object_dict)
+
+    qmodel, path = _quantize(model=model, cfg=cfg, datamodule=datamodule, trainer=trainer)
+    log.info(f"Model size after quantization: {get_model_size_mb(model):.3f} MB")
+
+    if cfg.get("test"):
+        log.info("Starting testing!")
+        trainer.accelerator = "cpu"
+        datamodule.batch_size_per_device = 1
+        trainer.test(model=qmodel, datamodule=datamodule, ckpt_path=path)
+        log.info(f"Best ckpt path: {path}")
+
+    test_metrics = trainer.callback_metrics
+
+    # merge train and test metrics
+    metric_dict = {**test_metrics}
+
+    return metric_dict, object_dict
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="quantize.yaml")
+def main(cfg: DictConfig) -> Optional[float]:
+    """Main entry point for training.
+
+    :param cfg: DictConfig configuration composed by Hydra.
+    :return: Optional[float] with optimized metric value.
+    """
+    extras(cfg)
+    metric_dict, _ = quantize(cfg)
+    metric_value = get_metric_value(metric_dict=metric_dict, metric_name=cfg.get("optimized_metric"))
+    return metric_value
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/quantization.py
@@ -0,0 +1,153 @@
+import os
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+import torch
+import torch.ao.quantization.quantize_fx as quantize_fx
+from pydantic import BaseModel
+from tinynn.graph.quantization.quantizer import QATQuantizer
+from torch.utils.data import DataLoader
+
+
+class QuantizerConfig(BaseModel):
+    """Config for PTQ/QAT quantizers."""
+
+    dummy_input_shape: List[int]
+    is_per_tensor: bool
+    is_asymmetric: bool = True
+    backend: str = "qnnpack"
+    disable_requantization_for_cat: bool = True
+
+
+def _ptq_calibration(
+    model: torch.nn.Module, examples_limit: int, dataloader: DataLoader, device: torch.device
+) -> torch.nn.Module:
+    """Performs simple ptq calibration pipeline.
+
+    Args:
+        model (torch.nn.Module): torch model to quantize (with enabled ptq observer)
+        examples_limit (int): how many samples from training data we will use to observe data vals
+        dataloader(Dataloader): traininig data loader for calibration
+        device (torch.device): on what device (cuda/cpu) we will do calibration
+
+    Returns:
+        torch.nn.Module: ptq calibrated model
+    """
+    for idx, data in enumerate(dataloader):
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        model(images)
+        if idx >= examples_limit:
+            break
+    return model
+
+
+def get_model_size_mb(model: torch.nn.Module) -> float:
+    """Returns size (on disk) of specific pytorch model.
+
+    Args:
+        model (torch.nn.Module): torch model
+
+    Returns:
+        float: model disk size in MB
+    """
+    torch.save(model.state_dict(), "temp.p")
+    size = os.path.getsize("temp.p") / 1e6
+    os.remove("temp.p")
+    return size
+
+
+def quantize_fuse_bn(model: torch.nn.Module) -> torch.nn.Module:
+    """For specific model, it fuses its modules like conv+bn, conv+bn+relu."""
+    return quantize_fx.fuse_fx(model.eval())
+
+
+def create_quantizer(model: torch.nn.Module, quant_config: QuantizerConfig, work_dir: Path) -> QATQuantizer:
+    """Creates quantizer (tiny neural network).
+
+    Args:
+        model (torch.nn.Module): torch model to quantize
+
+    Returns:
+        torch.nn.Module: model with performed bn fusing
+    """
+    quantizer = QATQuantizer(
+        model=model,
+        dummy_input=torch.randn(*quant_config.dummy_input_shape),
+        work_dir=work_dir,
+        config={
+            "asymmetric": quant_config.is_asymmetric,
+            "backend": quant_config.backend,
+            "disable_requantization_for_cat": quant_config.disable_requantization_for_cat,
+            "per_tensor": quant_config.is_per_tensor,
+        },
+    )
+    return quantizer
+
+
+def quantize_PTQ(
+    model: torch.nn.Module,
+    work_dir: Path,
+    quant_config: QuantizerConfig,
+    n_ptq_examples: int,
+    device: torch.device,
+    dataloader: DataLoader,
+    quantizer: Optional[QATQuantizer] = None,
+) -> Tuple[torch.nn.Module, QATQuantizer]:
+    """Performs Post Training Quantization (PTQ).
+
+    Args:
+        model (torch.nn.Module): torch model to quantize
+        work_dir (str): where output of quantization should be stored
+        quant_config (QuantizerConfig): config of quantizer
+        n_ptq_examples (int): how many samples from training data we will use to observe data vals
+        device (torch.device): on what device (cuda/cpu) we will do calibration
+        dataloader(Dataloader): traininig data loader for calibration
+        quantizer (Optional[QATQuantizer]): if passed, then we don't reinit quantizer. Defaults to None.
+
+    Returns:
+        torch.nn.Module: model after ptq
+        QATQuantizer: tinynn quantizer class
+    """
+    if quantizer is None:
+        quantizer = create_quantizer(model=model, quant_config=quant_config, work_dir=work_dir)
+        qmodel = quantizer.quantize()
+    else:
+        qmodel = model
+    qmodel = qmodel.eval()
+    qmodel = _ptq_calibration(model=qmodel, examples_limit=n_ptq_examples, dataloader=dataloader, device=device)
+    return qmodel, quantizer
+
+
+def prepare_for_QAT(
+    model: torch.nn.Module,
+    work_dir: str,
+    quant_config: QuantizerConfig,
+    quantizer: Optional[QATQuantizer] = None,
+) -> Tuple[torch.nn.Module, QATQuantizer]:
+    """Prepares model for QAT (Quantization aware training).
+
+    Args:
+        model (torch.nn.Module): torch model to quantize
+        work_dir (str): where output of quantization should be stored
+        quant_config (QuantizerConfig): config of quantizer
+        quantizer (Optional[QATQuantizer]): if passed, then we don't reinit quantizer. Defaults to None.
+
+    Returns:
+        torch.nn.Module: model prepared for qat
+        QATQuantizer: tinynn quantizer class
+    """
+    if quantizer is None:
+        quantizer = create_quantizer(model=model, quant_config=quant_config, work_dir=work_dir)
+        qmodel = quantizer.quantize()
+    else:
+        qmodel = model
+    qmodel = qmodel.eval()
+    qmodel = qmodel.apply(torch.quantization.disable_observer)
+    return qmodel, quantizer
+
+
+def get_model_quantized(model: torch.nn.Module) -> torch.nn.Module:
+    model = model.cpu()
+    model = model.eval()
+    return torch.quantization.convert(model.cpu(), inplace=False)
